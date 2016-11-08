972e88cee9825b9664538849731b62a996e2cd19
Jacob <jakerein@gmail.com>
2016-11-08 15:46:07 -0500

    Integration
    Starting to integrate Google Sheets Api into program
    
    Good Luck Jacob…….We’re gunna need it

--- src/main/java/com/excel/excel/com/GoogleSheets.java
+++ src/main/java/com/excel/excel/com/GoogleSheets.java
@@ -1,5 +1,11 @@
 package com.excel.excel.com;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
@@ -7,38 +13,34 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.sheets.v4.SheetsScopes;
-import com.google.api.services.sheets.v4.model.*;
 import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.api.services.sheets.v4.model.Sheet;
+import com.google.api.services.sheets.v4.model.Spreadsheet;
+import com.google.api.services.sheets.v4.model.ValueRange;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.List;
-
-public class Quickstart {
-
+public class GoogleSheets {
+	
 	/** Application name. */
-	private static final String APPLICATION_NAME = "Google Sheets API Java Quickstart";
+	private final String APPLICATION_NAME = "Google Sheets API Java Quickstart";
 
-	static String sheetID = "1jQzehm5To1ZDM-m0B3mI6EfFChny6rFCDlP2Svn_6o0";
+	String sheetID = "1jQzehm5To1ZDM-m0B3mI6EfFChny6rFCDlP2Svn_6o0";
 
 	/** Directory to store user credentials for this application. */
-	private static final java.io.File DATA_STORE_DIR = new java.io.File(System.getProperty("user.home"),
+	private final static java.io.File DATA_STORE_DIR = new java.io.File(System.getProperty("user.home"),
 			".credentials/sheets.googleapis.com-java-quickstart");
 
 	/** Global instance of the {@link FileDataStoreFactory}. */
 	private static FileDataStoreFactory DATA_STORE_FACTORY;
 
 	/** Global instance of the JSON factory. */
-	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+	private final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
 	/** Global instance of the HTTP transport. */
-	private static HttpTransport HTTP_TRANSPORT;
+	private static  HttpTransport HTTP_TRANSPORT;
 
 	/**
 	 * Global instance of the scopes required by this quickstart.
@@ -46,7 +48,7 @@ public class Quickstart {
 	 * If modifying these scopes, delete your previously saved credentials at
 	 * ~/.credentials/sheets.googleapis.com-java-quickstart
 	 */
-	private static final List<String> SCOPES = Arrays.asList(SheetsScopes.SPREADSHEETS_READONLY);
+	private final List<String> SCOPES = Arrays.asList(SheetsScopes.SPREADSHEETS_READONLY);
 
 	static {
 		try {
@@ -57,6 +59,48 @@ public class Quickstart {
 			System.exit(1);
 		}
 	}
+	
+	public GoogleSheets() throws IOException {
+		// Build a new authorized API client service.
+				Sheets service = getSheetsService();
+
+				// Prints the names and majors of students in a sample spreadsheet:
+				// https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+				String spreadsheetId = sheetID;
+
+				Spreadsheet response1 = service.spreadsheets().get(spreadsheetId).setIncludeGridData(false).execute();
+
+				List<Sheet> workSheetList = response1.getSheets();
+
+				for (Sheet sheet : workSheetList) {
+					System.err.println(sheet.getProperties().getTitle());
+
+					String range = sheet.getProperties().getTitle() + "!A1:BG20";// "A4:E20";//BG
+					ValueRange response = service.spreadsheets().values().get(spreadsheetId, range).execute();
+					List<List<Object>> values = response.getValues();
+					if (values == null || values.size() == 0) {
+						System.out.println("No data found.");
+					} else {
+						System.out.println("Size is: " + values.size());
+
+						for (int i = 0; i < values.size(); i++) {
+							for (int j = 0; j < values.get(i).size(); j++) {
+								System.out.print(values.get(i).get(j) + "\t");
+							}
+							System.out.println();
+						}
+
+						/*
+						 * for (List row : values) { // Print columns A and E, which
+						 * correspond to indices 0 and 4.
+						 * //System.out.printf("%s, %s\n", row.get(0), row.get(2));
+						 * System.out.println(row.get(0)); }
+						 */
+					}
+
+				}
+	}
+	
 
 	/**
 	 * Creates an authorized Credential object.
@@ -64,7 +108,7 @@ public class Quickstart {
 	 * @return an authorized Credential object.
 	 * @throws IOException
 	 */
-	public static Credential authorize() throws IOException {
+	public Credential authorize() throws IOException {
 		// Load client secrets.
 		InputStream in = Quickstart.class.getResourceAsStream("/client_secret.json");
 		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
@@ -83,51 +127,11 @@ public class Quickstart {
 	 * @return an authorized Sheets API client service
 	 * @throws IOException
 	 */
-	public static Sheets getSheetsService() throws IOException {
+	public Sheets getSheetsService() throws IOException {
 		Credential credential = authorize();
 		return new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).setApplicationName(APPLICATION_NAME)
 				.build();
 	}
-
-	public static void main(String[] args) throws IOException {
-		// Build a new authorized API client service.
-		Sheets service = getSheetsService();
-
-		// Prints the names and majors of students in a sample spreadsheet:
-		// https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-		String spreadsheetId = sheetID;
-
-		Spreadsheet response1 = service.spreadsheets().get(spreadsheetId).setIncludeGridData(false).execute();
-
-		List<Sheet> workSheetList = response1.getSheets();
-
-		for (Sheet sheet : workSheetList) {
-			System.err.println(sheet.getProperties().getTitle());
-
-			String range = sheet.getProperties().getTitle() + "!A1:BG20";// "A4:E20";//BG
-			ValueRange response = service.spreadsheets().values().get(spreadsheetId, range).execute();
-			List<List<Object>> values = response.getValues();
-			if (values == null || values.size() == 0) {
-				System.out.println("No data found.");
-			} else {
-				System.out.println("Size is: " + values.size());
-
-				for (int i = 0; i < values.size(); i++) {
-					for (int j = 0; j < values.get(i).size(); j++) {
-						System.out.print(values.get(i).get(j) + "\t");
-					}
-					System.out.println();
-				}
-
-				/*
-				 * for (List row : values) { // Print columns A and E, which
-				 * correspond to indices 0 and 4.
-				 * //System.out.printf("%s, %s\n", row.get(0), row.get(2));
-				 * System.out.println(row.get(0)); }
-				 */
-			}
-
-		}
-	}
-
+	
+	
 }
---
+++ src/main/java/com/excel/excel/com/Test.java
@@ -0,0 +1,19 @@
+package com.excel.excel.com;
+
+import java.io.IOException;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+			GoogleSheets gs = new GoogleSheets();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
